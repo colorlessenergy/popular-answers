c9eb07896f35b91bdac44700f0abfebcdc195253
colorlessenergy <brianmunozdev@gmail.com>
2021-09-18 08:57:26 -0700

    add functionality to add home page link

--- shared/components/Nav.js
+++ shared/components/Nav.js
@@ -1,8 +1,15 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
-export default function Nav () {
+export default function Nav ({ includeHomePage = false }) {
     return (
         <nav className="text-align-right">
+            { includeHomePage ? (
+                <Link href="/">
+                    <a>Home</a>
+                </Link>
+            ) : (null) }
+
             <Image
                 src="/assets/gear.svg"
                 alt="gear icon"
