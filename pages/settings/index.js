5cb7a610c161ee70a3eea5eb99273fbea04d8ba2
colorlessenergy <brianmunozdev@gmail.com>
2021-09-18 09:01:50 -0700

    create settings page and add nav component

---
+++ pages/settings/index.js
@@ -0,0 +1,9 @@
+import Nav from "../../shared/components/Nav";
+
+export default function Settings () {
+    return (
+        <div className="container">
+            <Nav includeHomePage={ true } />
+        </div>
+    );
+}
