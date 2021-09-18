bd6108641b402d7317ecab9807576d28a7c8c5d6
colorlessenergy <brianmunozdev@gmail.com>
2021-09-18 13:29:51 -0700

    add a title tag and meta description

--- pages/settings/index.js
+++ pages/settings/index.js
@@ -1,5 +1,8 @@
-import Link from "next/link";
-import Nav from "../../shared/components/Nav";
+import { Fragment } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+
+import Nav from '../../shared/components/Nav';
 
 export default function Settings () {
     const handleDarkMode = () => {
@@ -25,31 +28,37 @@ export default function Settings () {
     }
 
     return (
-        <div className="container">
-            <Nav includeHomePage={ true } />
+        <Fragment>
+            <Head>
+                <title>settings - popular answers</title>
+                <meta name="description" content="settings - popular answers" />
+            </Head>
+            <div className="container">
+                <Nav includeHomePage={ true } />
 
-            <Link href="/settings/all-questions">
-                <a className="text text-bold text-underline">
-                    all questions
-                </a> 
-            </Link>
+                <Link href="/settings/all-questions">
+                    <a className="text text-bold text-underline">
+                        all questions
+                    </a>
+                </Link>
 
-            <div className="flex justify-content-between align-items-center mt-2">
-                <label
-                    htmlFor="dark-mode"
-                    className="text text-bold text-underline cursor-pointer">
-                    dark mode
-                </label>
-                <input
-                    type="checkbox"
-                    className="d-none dark-mode-input"
-                    onClick={ handleDarkMode }
-                    defaultChecked={ isDefaultChecked() }
-                    id="dark-mode" />
-                <label
-                    htmlFor="dark-mode"
-                    className="dark-mode-checkbox cursor-pointer"></label>
+                <div className="flex justify-content-between align-items-center mt-2">
+                    <label
+                        htmlFor="dark-mode"
+                        className="text text-bold text-underline cursor-pointer">
+                        dark mode
+                    </label>
+                    <input
+                        type="checkbox"
+                        className="d-none dark-mode-input"
+                        onClick={ handleDarkMode }
+                        defaultChecked={ isDefaultChecked() }
+                        id="dark-mode" />
+                    <label
+                        htmlFor="dark-mode"
+                        className="dark-mode-checkbox cursor-pointer"></label>
+                </div>
             </div>
-        </div>
+        </Fragment>
     );
 }
