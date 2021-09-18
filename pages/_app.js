c33f5816ca227b91541b0192e555bbca5b9a1010
colorlessenergy <brianmunozdev@gmail.com>
2021-09-18 11:37:19 -0700

    add functionality to set theme to dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.scss'
+import { useEffect } from 'react';
+
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    useEffect(() => {
+        if (!localStorage.getItem('theme')) {
+            localStorage.setItem('theme', 'light')
+        }
+
+        if (localStorage.getItem('theme') === 'dark') {
+            document.body.classList.add('dark')
+        }
+    }, []);
+    
+    return <Component {...pageProps} />
 }
 
 export default MyApp
